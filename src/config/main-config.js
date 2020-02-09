8b5424d25d77c376b0574e8f532df2e3ec68946f
Ajetski <ajeniski4@gmail.com>
2020-02-09 13:30:00 -0500

    update paths

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -5,8 +5,8 @@
   
     appConfig.init = function (app, express) {
         // Define paths for Express config
-        const publicDirectoryPath = path.join(__dirname, './client/public')
-        const viewsDirectoryPath = path.join(__dirname, './client/views')
+        const publicDirectoryPath = path.join(__dirname, '../client/public')
+        const viewsDirectoryPath = path.join(__dirname, '../client/views')
 
         // Setup handlebars engine
         app.engine('.hbs', hbs({extname: '.hbs'}))
