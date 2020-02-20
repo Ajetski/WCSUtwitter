6593f122ba136a56d2b663e376e5d50a7017f592
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-02-20 00:56:41 -0500

    rename files, create basic routes

--- api/config/route-config.js
+++ api/config/route-config.js
@@ -1,10 +1,12 @@
 (function (routeConfig) {
-    routeConfig.init = function (app) {
-      // *** routes *** //
-      const baseRoutes = require('../routes/base.js');
-  
-      // *** register routes *** //
-      app.use('/', baseRoutes);
-  
-    };
-  }(module.exports));
+	routeConfig.init = function (app) {
+		// *** routes *** //
+		const testRoutes = require('../routes/test.js');
+		const userRoutes = require('../routes/user.js');
+
+		// *** register routes *** //
+		app.use('/api/test', testRoutes);
+		app.use('/api/user', userRoutes);
+
+	};
+}(module.exports));
