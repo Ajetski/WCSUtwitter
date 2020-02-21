99cb8ceb48de1fd2e672ba24cd047844063a2749
Ajetski <ajeniski4@gmail.com>
2020-02-21 16:32:17 -0500

    add connection pooling

--- api/db/db.js
+++ api/db/db.js
@@ -1,9 +1,22 @@
-const pgp = require('pg-promise')({
-    // Initialization Options: http://vitaly-t.github.io/pg-promise/module-pg-promise.html
-})
+const dbConnectionCreator = require('./dbConnector.js')
 
-module.exports = function ({username, password, host,port,  database }) {
-    const connectionString = `postgresql://${username}:${password}`
-                           + `@${host}:${port}/${database}?application_name=LearnDB`;                       
-    return db = pgp(connectionString);
-};
+//this will be replaced with obtaining values for a secret file or enviornment variable
+const host = ('127.0.0.1').trim();
+const port = ('32776').trim(); 
+const database = ('testingdb').trim();
+const username = ('superuser').trim();
+const password = ('password').trim();
+
+const db = dbConnectionCreator({
+	username,
+	password,
+	host,
+	port,
+	database
+});
+
+process.on('exit', () => {
+    db.$pool.end();
+});
+
+module.exports = db;
