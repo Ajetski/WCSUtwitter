aa5cfb2634d113255f80094adf15d0ccb6e68258
Ajetski <ajeniski4@gmail.com>
2020-02-21 19:11:46 -0500

    implement patch

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,24 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/db.js");
 
+
+// Get a user's profile data
+router.get("/:username", (req, res) => {
+    db.any(
+		`SELECT username, firstname, lastname, email
+		FROM users
+		WHERE username = '${req.params.username}';`
+	).then((p) => {
+		if (p.length < 1) {
+			throw new Error(`User '${req.params.username}' could not be found.`);
+		}
+		return res.send(p[0]);
+	}).catch((error) => {
+		return res.status(404).send({"error": `${error.message}`});
+	});
+});
+
+
 // Create a new user
 router.post("/", (req, res) => {
 	/** request body will have the following fields:
@@ -15,7 +33,7 @@ router.post("/", (req, res) => {
 	 * PrivacySetting
 	 * NotificationSetting
 	 */
-
+	
 	db.any(
 		`INSERT INTO users (
 			id, username, firstname, lastname, email,
@@ -28,8 +46,8 @@ router.post("/", (req, res) => {
 			'${req.body.email}',
 			'${req.body.hashedpassword}',
 			${req.body.profpic || "NULL"},
-			${req.body.profpic ? `'${req.body.privacysetting}'` : "NULL"},
-			${req.body.profpic ? `'${req.body.notificationsetting}'` : "NULL"}
+			${req.body.privacysetting ? `'${req.body.privacysetting}'` : "NULL"},
+			${req.body.notificationsetting ? `'${req.body.notificationsetting}'` : "NULL"}
 		);`
 	).then((p) => {
 		return res.status(200).send({
@@ -43,38 +61,29 @@ router.post("/", (req, res) => {
 	});
 });
 
-// Get a user's profile data
-router.get("/:username", (req, res) => {
-    db.any(
-		`SELECT username, firstname, lastname, email
-		FROM users
-		WHERE username = '${req.params.username}';`
-	).then((p) => {
-		if (p.length < 1) {
-			throw new Error(`User '${req.params.username}' could not be found.`);
-		}
-		return res.send(p);
-	}).catch((error) => {
-		return res.status(404).send({"error": `${error}`});
-	});
-});
 
 // Update a user's profile data
 router.patch("/:username", (req, res) => {
-	/** To be implemented
-	console.log(`'${req.params.username}'`)
+	updateString = ''
+	for (const field in req.body) {
+		updateString = `${updateString}, ${field} = '${req.body[field]}'`
+	}
+	updateString = updateString.substr(1)
+	//return res.send(updateString)
+
     db.any(
-		`SELECT username, firstname, lastname, email
-		FROM users
+		`UPDATE users
+		SET ${updateString}
 		WHERE username = '${req.params.username}';`
 	).then((p) => {
-		return res.send(p);
+		return res.status(200).send({
+			response: `User '${req.body.username}' has been updated.`
+		});
 	}).catch((error) => {
 		return res.status(500).send(error);
 	});
-	 */
-	res.send('Not yet implemented.');
 });
+
 
 // Delete a user
 router.delete("/:username", (req, res) => {
