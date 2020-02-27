f2a2c9e26de2db217cae1f49de53b8209b619bc9
Ajetski <ajeniski4@gmail.com>
2020-02-27 18:58:14 -0500

    create Users model

---
+++ api/db/models/users.js
@@ -0,0 +1,46 @@
+const sequelize = require('../sequelize.js')
+const {Model, DataTypes } = require('sequelize');
+
+const Users = sequelize.define('users', {
+    id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    hashedpassword: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    profpic: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    privacysetting: {
+        type: DataTypes.JSON,
+        allowNull: true
+    },
+    notificationsetting: {
+        type: DataTypes.JSON,
+        allowNull: true
+    }
+}, {
+    timestamps: false
+})
+
+module.exports = Users;
