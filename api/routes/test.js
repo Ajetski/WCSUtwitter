a236d3c2455411e329abfcb758cb7753f32367f9
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-14 20:57:47 -0400

    update formatting

--- api/routes/test.js
+++ api/routes/test.js
@@ -1,28 +1,30 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+
+const { User } = require('../db/models');
 
 // Default route
 
-const multer = require('multer')
+const multer = require('multer');
 const upload = multer({
 	
-})
+});
 
-router.post("/", upload.single('picture'), (req, res) => {
+router.post('/', upload.single('picture'), (req, res) => {
 	console.log(req.file.buffer);
-	console.log(req.file)
+	console.log(req.file);
 	res.send({
 		body: req.body
 	});
 });
 
 
-router.get("/select", (req, res) => {
+router.get('/select', (req, res) => {
 	User.findOne().then(results => {
-		res.send(results)
+		res.send(results);
 	}).catch(error => {
-		res.status(500).send(error)
-	})
+		res.status(500).send(error);
+	});
 });
 
 module.exports = router;
