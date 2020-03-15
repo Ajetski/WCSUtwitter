f101a1af0f359f4fab07a4b6093c04c512758a6f
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-14 23:53:54 -0400

    remove 'belongsTo' fk insertion in models

--- api/db/models.js
+++ api/db/models.js
@@ -1,13 +1,9 @@
-const sequalize = require('./sequelize');
 const User = require('./models/users');
 const UserName = require('./models/username');
 const UserEmail = require('./models/useremail');
 
-UserName.belongsTo(User);
-UserEmail.belongsTo(User);
-
 module.exports = {
-    User,
+	User,
 	UserName,
 	UserEmail
 };
