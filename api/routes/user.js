e82a54fe9356be65d5b4f59492ac51533f4c0d54
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-14 23:59:07 -0400

    re-implement create and delete user, to-do: update and get

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,25 +1,32 @@
 //my modules
-const imageUpload = require("../util/image-upload");
-const {User} = require("../db/models.js");
+const imageUpload = require('../util/image-upload');
+const { User, UserName, UserEmail } = require('../db/models');
+const sequelize = require('../db/sequelize');
 
 //core modules
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
 //npm modules
-const sharp = require("sharp");
-const express = require("express");
+const sharp = require('sharp');
+const express = require('express');
 const router = express.Router();
 
 // Get a user's profile data
-router.get("/:username", async (req, res) => {
+router.get('/:username', async (req, res) => {
 	try{
+		const userId = await UserName.findOne({
+			attributes: ['id'],
+			where:{
+				username: req.params.username
+			}
+		});
 		const user = await User.findOne({
 			attributes: {
 				exclude: ['hashedpassword']
 			},
 			where:{
-				username: req.params.username
+				username: userId.id
 			}
 		});
 		if (!user)
@@ -32,7 +39,7 @@ router.get("/:username", async (req, res) => {
 });
 
 // Get a user's profile picture
-router.get("/:username/pic", async (req, res) => {
+router.get('/:username/pic', async (req, res) => {
 	try{
 		//find a user by their username
 		const user = await User.findOne({
@@ -47,12 +54,12 @@ router.get("/:username/pic", async (req, res) => {
 		//if the user has a profile picture, serve it
 		else if (user.profpic)
 			res.sendFile(
-				path.resolve(__dirname, "..", "uploaded_media", "user_profile_pics_large", `${req.params.username}.png`)
+				path.resolve(__dirname, '..', 'uploaded_media', 'user_profile_pics_large', `${req.params.username}.png`)
 			);
 		//if the user does not have a profile pic, serve the default
 		else
 			res.sendFile(
-				path.resolve(__dirname, "..", "uploaded_media", "default_profile_pic", "default.png")
+				path.resolve(__dirname, '..', 'uploaded_media', 'default_profile_pic', 'default.png')
 			);
 	}
 	catch(error){
@@ -61,8 +68,8 @@ router.get("/:username/pic", async (req, res) => {
 });
 
 // Create a new user
-router.post("/", imageUpload.single("profpic"), async (req, res) => {
-	if (typeof req.body.user !== "object"){
+router.post('/', imageUpload.single('profpic'), async (req, res) => {
+	if (typeof req.body.user !== 'object'){
 		req.body.user = JSON.parse(req.body.user);
 	}
 
@@ -74,10 +81,12 @@ router.post("/", imageUpload.single("profpic"), async (req, res) => {
 		req.body.user.profpic = true;
 
 		try{
-			await fs.unlink(path.resolve("uploaded_media", "user_profile_pics_large", req.params.username) + '.png', err => {});
-			await fs.unlink(path.resolve("uploaded_media", "user_profile_pics_medium", req.params.username) + '.png', err => {});
-			await fs.unlink(path.resolve("uploaded_media", "user_profile_pics_small", req.params.username) + '.png', err => {});
-		} catch (err) {}
+			await fs.unlink(path.resolve('uploaded_media', 'user_profile_pics_large', req.body.user.username) + '.png', () => {});
+			await fs.unlink(path.resolve('uploaded_media', 'user_profile_pics_medium', req.body.user.username) + '.png', () => {});
+			await fs.unlink(path.resolve('uploaded_media', 'user_profile_pics_small', req.body.user.username) + '.png', () => {});
+		} catch (err) {
+			console.log('Error', err);
+		}
 
 		try {
 			//resize and save large image
@@ -85,7 +94,7 @@ router.post("/", imageUpload.single("profpic"), async (req, res) => {
 				.resize({ width: 500, height: 500 })
 				.png()
 				.toFile(
-					path.resolve(req.file.destination, "user_profile_pics_large", image)
+					path.resolve(req.file.destination, 'user_profile_pics_large', image)
 				);
 
 			//resize and save medium image
@@ -93,7 +102,7 @@ router.post("/", imageUpload.single("profpic"), async (req, res) => {
 				.resize({ width: 250, height: 250 })
 				.png()
 				.toFile(
-					path.resolve(req.file.destination, "user_profile_pics_medium", image)
+					path.resolve(req.file.destination, 'user_profile_pics_medium', image)
 				);
 			
 			//resize and save small image
@@ -101,31 +110,65 @@ router.post("/", imageUpload.single("profpic"), async (req, res) => {
 				.resize({ width: 50, height: 50 })
 				.png()
 				.toFile(
-					path.resolve(req.file.destination, "user_profile_pics_small", image)
+					path.resolve(req.file.destination, 'user_profile_pics_small', image)
 				);
 
-			await fs.unlink(req.file.path, err => {});
+			fs.unlink(req.file.path, () => { });
 		} catch (error) {
 			//if image resizeing and saving fails, handle error
-			console.log("Error:", error);
+			console.log('Error:', error);
 			return res.status(500).send({
 				error,
 				response: `User ${req.body.user.username} could not be created.`
 			});
 		}
 	}
+	//if there is no profpic, then the DB will save a false value
 	else
 		req.body.user.profpic = false;
+	
+	// create a transaction to be commited or rolled back later
+	const transaction = await sequelize.transaction();
 
 	try{
-		await User.create(req.body.user);
-		res.send({
+		//destructure the variables needed from req.body.user
+		const {username, firstname, lastname, email, hashedpassword, profpic} = req.body.user;
+
+		//create an instance in the user table
+		const user = await User.create({
+			firstname,
+			lastname,
+			hashedpassword,
+			profpic,
+		});
+
+		//save the user's id from the new instance in users table
+		const userId = user.dataValues.id;
+
+		//create an instance in the username table w fk to users
+		await UserName.create({
+			id: userId,
+			username
+		});
+
+		//create an instance in the useremail table w fk to users
+		await UserEmail.create({
+			id: userId,
+			email
+		});
+
+		await transaction.commit();
+
+		return res.send({
 			response: `User ${req.body.user.username} has been created.`,
 			profpic: req.body.user.profpic
 		});
+
 	}
 	catch (error) {
-		res.status(500).send({
+		await transaction.rollback();
+
+		return res.status(500).send({
 			error: error,
 			response: `User ${req.body.user.username} could not be created.`
 		});
@@ -133,8 +176,8 @@ router.post("/", imageUpload.single("profpic"), async (req, res) => {
 });
 
 // Update a user's profile data
-router.patch("/:username", imageUpload.single("profpic"), async (req, res) => {
-	if (typeof req.body.user !== "object"){
+router.patch('/:username', imageUpload.single('profpic'), async (req, res) => {
+	if (typeof req.body.user !== 'object'){
 		req.body.user = JSON.parse(req.body.user);
 	}
 
@@ -145,10 +188,12 @@ router.patch("/:username", imageUpload.single("profpic"), async (req, res) => {
 	if (image) {
 		req.body.user.profpic = true;
 		try{
-			await fs.unlink(path.resolve("uploaded_media", "user_profile_pics_large", req.params.username) + '.png', err => {});
-			await fs.unlink(path.resolve("uploaded_media", "user_profile_pics_medium", req.params.username) + '.png', err => {});
-			await fs.unlink(path.resolve("uploaded_media", "user_profile_pics_small", req.params.username) + '.png', err => {});
-		} catch (err) {}
+			await fs.unlink(path.resolve('uploaded_media', 'user_profile_pics_large', req.params.username) + '.png', () => {});
+			await fs.unlink(path.resolve('uploaded_media', 'user_profile_pics_medium', req.params.username) + '.png', () => {});
+			await fs.unlink(path.resolve('uploaded_media', 'user_profile_pics_small', req.params.username) + '.png', () => {});
+		} catch (err) {
+			console.log('Error', err);
+		}
 
 		try {
 			//resize and save large image
@@ -156,7 +201,7 @@ router.patch("/:username", imageUpload.single("profpic"), async (req, res) => {
 				.resize({ width: 500, height: 500 })
 				.png()
 				.toFile(
-					path.resolve(req.file.destination, "user_profile_pics_large", image)
+					path.resolve(req.file.destination, 'user_profile_pics_large', image)
 				);
 
 			//resize and save medium image
@@ -164,7 +209,7 @@ router.patch("/:username", imageUpload.single("profpic"), async (req, res) => {
 				.resize({ width: 250, height: 250 })
 				.png()
 				.toFile(
-					path.resolve(req.file.destination, "user_profile_pics_medium", image)
+					path.resolve(req.file.destination, 'user_profile_pics_medium', image)
 				);
 			
 			//resize and save small image
@@ -172,10 +217,10 @@ router.patch("/:username", imageUpload.single("profpic"), async (req, res) => {
 				.resize({ width: 50, height: 50 })
 				.png()
 				.toFile(
-					path.resolve(req.file.destination, "user_profile_pics_small", image)
+					path.resolve(req.file.destination, 'user_profile_pics_small', image)
 				);
 
-			await fs.unlink(req.file.path, err => {});
+			await fs.unlink(req.file.path, () => {});
 		} catch (error) {
 			//if image resizeing and saving fails, handle error
 			return res.status(500).send({
@@ -205,15 +250,30 @@ router.patch("/:username", imageUpload.single("profpic"), async (req, res) => {
 });
 
 // Delete a user
-router.delete("/:username", async (req, res) => {
-	await fs.unlink(path.resolve("uploaded_media", "user_profile_pics_large", req.params.username) + '.png', err => {});
-	await fs.unlink(path.resolve("uploaded_media", "user_profile_pics_medium", req.params.username) + '.png', err => {});
-	await fs.unlink(path.resolve("uploaded_media", "user_profile_pics_small", req.params.username) + '.png', err => {});
+router.delete('/:username', async (req, res) => {
+	await fs.unlink(path.resolve('uploaded_media', 'user_profile_pics_large', req.params.username) + '.png', () => {});
+	await fs.unlink(path.resolve('uploaded_media', 'user_profile_pics_medium', req.params.username) + '.png', () => {});
+	await fs.unlink(path.resolve('uploaded_media', 'user_profile_pics_small', req.params.username) + '.png', () => {});
 
 	try{
-		await User.destroy({
+		const userId = await UserName.findOne({
 			where: {
 				username: req.params.username
+			}
+		}).get('id');
+		await UserEmail.destroy({
+			where: {
+				id: userId
+			}
+		});
+		await UserName.destroy({
+			where: {
+				id: userId
+			}
+		});
+		await User.destroy({
+			where: {
+				id: userId
 			}
 		});
 		return res.send({response: `User ${req.params.username} has been deleted.`});
