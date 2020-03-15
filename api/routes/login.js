5759e3851faf016437510425d83b64f918f34176
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-14 23:58:26 -0400

    update formatting

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,13 +1,13 @@
 //my modules
-const {User} = require("../db/models.js");
+const {User} = require('../db/models.js');
 
 //npm modules
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 
 // Get a user's profile data
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
 	try{
 		const user = await User.findOne({
 			where: {
@@ -16,11 +16,11 @@ router.post("/", async (req, res) => {
 			}
 		});
 		if (!user)
-				return res.status(404).send({error: `User ${req.params.username} not found.`});
-			return res.send({
-				response: `Login attempt for user '${req.body.username}' was a success.`,
-				jwt: "1234567890" //fake jwt, need to implement real jwt
-			});
+			return res.status(404).send({error: `User ${req.params.username} not found.`});
+		return res.send({
+			response: `Login attempt for user '${req.body.username}' was a success.`,
+			jwt: '1234567890' //fake jwt, need to implement real jwt
+		});
 	}
 	catch(error) {
 		return res.status(500).send({
