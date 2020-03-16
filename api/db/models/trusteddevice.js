20d4e56a545d7a0083f0b3a74631817de01ca4d5
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-16 10:48:45 -0400

    init trusteddevice.js

---
+++ api/db/models/trusteddevice.js
@@ -0,0 +1,25 @@
+const sequelize = require('../sequelize');
+
+const { DataTypes } = require('sequelize');
+
+const STRING = DataTypes.STRING;
+const INTEGER = DataTypes.INTEGER;
+
+const TrustedDevice = sequelize.define('trusteddevice', {
+	userid: {
+		primaryKey: true,
+		type: INTEGER,
+		autoIncrement: true,
+		allowNull: false
+	},
+	authtoken: {
+		primaryKey: true,
+		type: STRING,
+		allowNull: false
+	}
+}, {
+	timestamps: false,
+	tableName: 'trusteddevice'
+});
+
+module.exports = TrustedDevice;
