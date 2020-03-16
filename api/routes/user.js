1b9d92183d826bc66a9e949aeb99b2c11fff04b2
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-16 14:07:38 -0400

    remove unecessary console log

--- api/routes/user.js
+++ api/routes/user.js
@@ -14,7 +14,6 @@ const router = express.Router();
 
 // Get a user's profile data
 router.get('/:username', async (req, res) => {
-	console.log(req.params.username);
 	try{
 		const username = await UserName.findOne({
 			attributes: ['userid'],
