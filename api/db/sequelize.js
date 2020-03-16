c66a119e408547e853b61d65dd3efd806b492a66
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-16 11:06:10 -0400

    add comments explaining how to use dotenv

--- api/db/sequelize.js
+++ api/db/sequelize.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+// For the following line to work, you must either have a .env file with the following format:
+// DB_CONNECTION_STRING=postgres://example
+// see .env.example file in the parent directory of this file
 const sequelize = new Sequelize(`${process.env.DB_CONNECTION_STRING}`);
 
 // log if connection is established
