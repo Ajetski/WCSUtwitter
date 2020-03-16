19c3fcf05965a4b39f554e09d50a241758c1606e
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-16 10:37:05 -0400

    use dotenv to hide connection string

--- api/db/sequelize.js
+++ api/db/sequelize.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('postgres://superuser:password@127.0.0.1:32776/testingdb');
+const sequelize = new Sequelize(`${process.env.DB_CONNECTION_STRING}`);
 
 // log if connection is established
 sequelize.authenticate().then(() => console.log('DB connected')).catch(err => console.log('DB error:', err));
