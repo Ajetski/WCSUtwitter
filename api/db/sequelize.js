84c48c7e5d642a7afed6bdf019dbfb742b440f50
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-16 14:22:20 -0400

    remove unecessary formatted string

--- api/db/sequelize.js
+++ api/db/sequelize.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // For the following line to work, you must either have a .env file with the following format:
 // DB_CONNECTION_STRING=postgres://example
 // see .env.example file in the parent directory of this file
-const sequelize = new Sequelize(`${process.env.DB_CONNECTION_STRING}`);
+const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
 
 // log if connection is established
 sequelize.authenticate().then(() => console.log('DB connected')).catch(err => console.log('DB error:', err));
