920047d15d7513b4ed7b15bb3864f9deb742f0ae
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-26 01:07:24 -0400

    export loggin session

--- api/db/models.js
+++ api/db/models.js
@@ -1,9 +1,11 @@
 const User = require('./models/users');
 const UserName = require('./models/username');
 const UserEmail = require('./models/useremail');
+const LoginSession = require('./models/loginsession');
 
 module.exports = {
 	User,
 	UserName,
-	UserEmail
+	UserEmail,
+	LoginSession
 };
