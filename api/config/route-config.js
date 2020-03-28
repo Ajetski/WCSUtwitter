07e3906329a5eb3bbeaad9f2a80b449e92f3683b
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-28 17:45:31 -0400

    add block and follow routes to app

--- api/config/route-config.js
+++ api/config/route-config.js
@@ -6,6 +6,8 @@
 		const usersRoutes = require('../routes/users');
 		const loginRoutes = require('../routes/login');
 		const logoutRoutes = require('../routes/logout');
+		const followRoutes = require('../routes/follow');
+		const blockRoutes = require('../routes/block');
 
 		// *** register routes *** //
 		app.use('/api/test', testRoutes);
@@ -13,5 +15,7 @@
 		app.use('/api/users', usersRoutes);
 		app.use('/api/login', loginRoutes);
 		app.use('/api/logout', logoutRoutes);
+		app.use('/api/follow', followRoutes);
+		app.use('/api/block', blockRoutes);
 	};
 })(module.exports);
