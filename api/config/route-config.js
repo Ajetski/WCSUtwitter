84d5624aeafd18713950c8f0611fcfe9cc72bc51
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-28 02:28:39 -0400

    add users.js routes

--- api/config/route-config.js
+++ api/config/route-config.js
@@ -3,11 +3,13 @@
 		// *** routes *** //
 		const testRoutes = require('../routes/test.js');
 		const userRoutes = require('../routes/user.js');
+		const usersRoutes = require('../routes/users.js');
 		const loginRoutes = require('../routes/login.js');
 
 		// *** register routes *** //
 		app.use('/api/test', testRoutes);
 		app.use('/api/user', userRoutes);
+		app.use('/api/users', usersRoutes);
 		app.use('/api/login', loginRoutes);
 	};
 })(module.exports);
