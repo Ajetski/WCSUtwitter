56d36a32ea9ac9d1bf5489cf7b01a44b3946b5e3
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-28 17:28:53 -0400

    add logout router to app

--- api/config/route-config.js
+++ api/config/route-config.js
@@ -1,15 +1,17 @@
 (function(routeConfig) {
 	routeConfig.init = function(app) {
 		// *** routes *** //
-		const testRoutes = require('../routes/test.js');
-		const userRoutes = require('../routes/user.js');
-		const usersRoutes = require('../routes/users.js');
-		const loginRoutes = require('../routes/login.js');
+		const testRoutes = require('../routes/test');
+		const userRoutes = require('../routes/user');
+		const usersRoutes = require('../routes/users');
+		const loginRoutes = require('../routes/login');
+		const logoutRoutes = require('../routes/logout');
 
 		// *** register routes *** //
 		app.use('/api/test', testRoutes);
 		app.use('/api/user', userRoutes);
 		app.use('/api/users', usersRoutes);
 		app.use('/api/login', loginRoutes);
+		app.use('/api/logout', logoutRoutes);
 	};
 })(module.exports);
